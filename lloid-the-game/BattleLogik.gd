c1d83ceeaa5d02543ef179946dabeb41abb35912
Cr1stal7 <andrewleo2003@gmail.com>
2025-12-04 21:50:01 +0100

    Making character controls

--- lloid-the-game/BattleLogik.gd
+++ lloid-the-game/BattleLogik.gd
@@ -1,50 +1,15 @@
-extends Node
+extends CharacterBody2D
 
-var player_hp: int = 30
-var enemy_hp: int = 20
-
-var turn: String = "player"  # "player" или "enemy"
-
-@onready var attack_button = $"../Ui/AttackButton"
-@onready var info_label = $"../Ui/InfoLabel"
-
-func _ready():
-	update_ui()
-	attack_button.pressed.connect(_on_player_attack_pressed)
-
-func update_ui():
-	info_label.text = "HP игрока: %s   HP врага: %s\nХод: %s" % [
-		player_hp, enemy_hp, turn
-	]
-	
-
-func _on_player_attack_pressed():
-	if turn != "player":
-		return
-
-	enemy_hp -= randi_range(4, 8)
-	if enemy_hp <= 0:
-		enemy_hp = 0
-		update_ui()
-		info_label.text += "\nПобеда!"
-		attack_button.disabled = true
-		return
-
-	turn = "enemy"
-	update_ui()
-	await enemy_turn()
-
-func enemy_turn():
-	await get_tree().create_timer(0.8).timeout  # небольшая задержка
-
-	player_hp -= randi_range(2, 6)
-
-	if player_hp <= 0:
-		player_hp = 0
-		update_ui()
-		info_label.text += "\nПоражение!"
-		attack_button.disabled = true
-		return
-
-	turn = "player"
-	update_ui()
+@export var walk_speed := 180.0
+@export var sprint_multiplier := 1.6
+func _physics_process(_delta):
+	var dir = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+	var speed := walk_speed
+	if Input.is_action_pressed("Sprint"):
+		speed *= sprint_multiplier
+	velocity = dir * speed
+	move_and_slide()
+				
